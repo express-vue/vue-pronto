30562c0728b35ef8163898d9e879ad00d66da26a
Daniel Cherubini <daniel@cherubini.casa>
2017-12-20 14:36:43 +0100

    improving coverage

--- tests/renderer.js
+++ tests/renderer.js
@@ -3,7 +3,7 @@ const test = require("ava");
 const path = require("path");
 const Renderer = require("../lib");
 
-const vueFile = path.join(__dirname, "example/test.vue");
+const vueFile = path.join(__dirname, "example/test2.vue");
 
 
 //@ts-ignore
@@ -19,7 +19,7 @@ test('String returns with full object', t => {
         title: "Test",
         template: templateLiteral
     };
-    const resultFull = `<!DOCTYPE html>\n<html lang="en">\n<head>\n<title>Test</title>\n<style>\n.red {\n  color: #f00;\n}\n</style>\n</head>\n<body>\n<h1>FOOOOO</h1>\n<h1 data-server-rendered="true" class="red">Hello world!</h1>\n</body>\n</html>`
+    const resultFull = `<!DOCTYPE html>\n<html lang="en">\n<head>\n<title>Test</title>\n<style>\n.red {\n  color: #f00;\n}\n</style>\n</head>\n<body>\n<h1>FOOOOO</h1>\n<div data-server-rendered="true"><h1 class="red">Hello world!</h1> <div><h2>Hello from component</h2> <h2>Hello from subcomponent</h2></div> <p>true</p> <div><p class="red">FAKEHTML</p></div></div>\n</body>\n</html>`
     return renderer.RenderToString(vueFile, data, vueOptions)
         .then(rendered => {
             t.is(rendered, resultFull);
@@ -32,7 +32,7 @@ test('String returns with full object', t => {
 //@ts-ignore
 test('String returns with no object', t => {
     const renderer = new Renderer({});
-    const resultHalf = `<!DOCTYPE html>\n<html lang="en">\n<head>\n<title></title>\n<style>\n.red {\n  color: #f00;\n}\n</style>\n</head>\n<body>\n<h1 data-server-rendered="true" class="red">Hello world!</h1>\n</body>\n</html>`
+    const resultHalf = `<!DOCTYPE html>\n<html lang="en">\n<head>\n<title></title>\n<style>\n.red {\n  color: #f00;\n}\n</style>\n</head>\n<body>\n<div data-server-rendered="true"><h1 class="red">Hello world!</h1> <div><h2>Hello from component</h2> <h2>Hello from subcomponent</h2></div> <p></p> <div></div></div>\n</body>\n</html>`
     return renderer.RenderToString(vueFile, {}, {})
         .then(rendered => {
             t.is(rendered, resultHalf);
