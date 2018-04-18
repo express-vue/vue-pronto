67e44825119b4431f681208992163de5ca44fd74
Daniel Cherubini <daniel@cherubini.casa>
2018-04-18 12:54:43 +0200

    Draft idea

--- tests/example/components/message-comp.vue
+++ tests/example/components/message-comp.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="">
       <h1>{{message}}</h1>
+      <p>{{destructure}}</p>
   </div>
 </template>
 
 <script>
 export default {
-    props: ['message']
+    props: ['message'],
+    computed: {
+        destructure: function() {
+
+            for (const [a, b] of Object.entries({a: "b"})) {
+                
+            }
+        }
+    }
 }
 </script>
