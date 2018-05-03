10d14d3bb995b330c1ea5cf5c5efa09d9b8d04cd
Daniel Cherubini <daniel@cherubini.casa>
2018-05-03 10:40:53 +0200

    updating

--- .expressvue/entry-server.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "./app"
-            export default context => {
-                return new Promise((resolve, reject) => {
-                    const { app } = createApp(context.state);
-                    resolve(app);
-                });
-            };
