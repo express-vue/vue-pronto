4091d8b63a6671b935ae4eca66b4f2fdebd32eef
Daniel Cherubini <daniel@cherubini.casa>
2018-05-22 12:45:49 +0200

    uipdating

--- .expressvue/tests/example/views/error/app.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-            import App from "/Users/danielcherubini/Coding/Express-Vue/vue-pronto/tests/example/views/error.vue";
-            export function createApp (state) {
-                const app = new Vue({
-                    render: h => h(App, {props: state})
-                })
-                return { app }
-            }
--- .expressvue/tests/example/views/error/entry-client.js
+++
@@ -1,4 +0,0 @@
-import { createApp } from "./app"
-            const state = window.__INITIAL_STATE__;
-            const { app } = createApp(state ? state : {});
-            app.$mount("#app")
